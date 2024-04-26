c975f442081e32c59592369f0f6c81af665afe9d
Gustavo Josue Robles Martinez <roblesmgustavoj@gmail.com>
2024-04-26 09:05:42 +0000

    cleanup

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+## Steps to run in Codespaces
+
 ##  apt update
 ##  apt install ghostscript python3-tk
 ##  apt-get install ffmpeg libsm6 libxext6
-# pip install camelot-py[cv]
-# pip install 'PyPDF2<3.0'
+##  pip install camelot-py[cv]
+##  pip install 'PyPDF2<3.0'
+
+# CONFIG section
+
+directory_path = 'ISC'
+csv_filename = directory_path + '.csv'
+
+# END CONFIG section
+
 import camelot
 import os
+import csv
 
 def list_files_in_subdirectory(directory):
     """
@@ -23,34 +34,6 @@ def list_files_in_subdirectory(directory):
             file_list.append(os.path.join(root, file))
     return file_list
 
-# Example usage:
-directory_path = 'ISC'
-files = list_files_in_subdirectory(directory_path)
-tableResults = []
-test = 0
-for file in files:
-    tables = camelot.read_pdf(file, pages='all')
-    # tables.export('foo.csv', f='csv', compress=True) # json, excel, html, sqlite
-    #tables[0]
-    #tables[0].parsing_report
-    #tables[0].to_csv('foo.csv') # to_json, to_excel, to_html, to_sqlite
-
-    # Consigue las tablas con la columna contenido
-    tablesContenido = []
-    for table in tables:
-        tempDF = table.df
-        if tempDF.shape[0] >= 2 and tempDF.shape[1] >= 2 and tempDF[1][0] == 'CONTENIDO':
-            tablesContenido.append( tempDF )
-    resultadosContenidop = ""
-    for table in tablesContenido:
-        resultadosContenidop = resultadosContenidop + table[1][2] + "\n"
-        #print( table[1][2] )
-    tableResults.append([file, resultadosContenidop])
-
-
-
-import csv
-
 def export_to_csv(data, filename):
     """
     Exports a 2D array into a CSV file.
@@ -63,10 +46,23 @@ def export_to_csv(data, filename):
         csv_writer = csv.writer(csvfile)
         csv_writer.writerows(data)
 
-# Example usage:
+# Get all the files in the directory_path
+files = list_files_in_subdirectory(directory_path)
 
+# Get all the tables with section "CONTENIDO" 
+tableResults = []
+for file in files:
+    tables = camelot.read_pdf(file, pages='all')
+    tablesContenido = []
+    for table in tables:
+        tempDF = table.df
+        if tempDF.shape[0] >= 2 and tempDF.shape[1] >= 2 and tempDF[1][0] == 'CONTENIDO':
+            tablesContenido.append( tempDF )
+    contentResult = ""
+    for table in tablesContenido:
+        contentResult = contentResult + table[1][2] + "\n"
+    tableResults.append([file, contentResult])
 
-csv_filename = 'ISC.csv'
 export_to_csv(tableResults, csv_filename)
 print("CSV file has been created:", csv_filename)
 
