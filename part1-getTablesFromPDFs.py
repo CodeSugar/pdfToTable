d90e525a064506218f6bf25110d3e3a01901e7de
Gustavo Robles <roblesmgustavoj@gmail.com>
2024-04-27 14:29:49 -0700

    Updated

--- part1-getTablesFromPDFs.py
+++ part1-getTablesFromPDFs.py
@@ -23,19 +23,38 @@ files = list_files_in_subdirectory(directory_path)
 
 # Get all the tables with section "CONTENIDO" and extract the information,
 #  save it in tableResults
-tableResults = [["FILE" , "CONTENIDO"]]
+tableResults = [["FILE" , "PROPOSITO","CONTENIDO", "PRACTICAS","PRACTICAS_UBICACION"]]
+unidadesTematicasResult = [["FILE", "NOMBRE", "UNIDAD_DE_COMPETENCIA","CONTENIDO"]]
+
 for file in files:
     tables = camelot.read_pdf(file, pages='all')
     tablesContenido = []
+    PRACTICAS=""
+    PRACTICAS_UBICACION=""
     for table in tables:
         tempDF = table.df
         if tempDF.shape[0] >= 2 and tempDF.shape[1] >= 2 and tempDF[1][0] == 'CONTENIDO':
             tablesContenido.append( tempDF )
+            unidadesTematicasResult.append( [file, tempDF[0][0], tempDF[0][2], tempDF[1][2] ])
+
+        if tempDF.shape[0] >= 1 and tempDF.shape[1] >= 1 and tempDF[0][0] == 'RELACIÓN DE PRÁCTICAS':
+            print(file)
+            print(tempDF[0][2])
+            print([ x.strip() for x in tempDF[0][2].split("\n")])
+            arrayTemp = [ x.strip().replace(".","") for x in tempDF[0][2].split("\n") if x.strip() != ""]
+            PRACTICAS = max([ int(x.strip()) for x  in arrayTemp])
+            PRACTICAS_UBICACION = tempDF[3][2]
+
+
     contentResult = ""
     for table in tablesContenido:
         contentResult = contentResult + table[1][2] + "\n"
-    tableResults.append([file, contentResult])
+
+    proposito = tables[1].df[0][0]
+    tableResults.append([file, proposito,  contentResult, PRACTICAS, PRACTICAS_UBICACION])
 
 export_to_csv(tableResults, csv_filename)
 print("CSV file has been created:", csv_filename)
+export_to_csv(unidadesTematicasResult, "unidadesTematicasResult.csv")
+print("CSV file has been created:", "unidadesTematicasResult.csv")
 
